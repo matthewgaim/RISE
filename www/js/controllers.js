e4c58762ffc6a4ef08e162dd45613bd9cf5b31db
Matthew Gaim <matthewgaim@gmail.com>
2016-06-25 16:48:17 -0700

    Like, Date
    - Like/Dislike button works
    - Date is shown for each post
    - When image is hold down, the post is deleted

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,9 @@
-angular.module('app.controllers', ["firebase"])
+var app = angular.module('app.controllers', ["firebase"]);
 
-
-
-.controller('cameraTabDefaultPageCtrl', function($scope,$cordovaCamera,$firebaseObject,$firebaseArray) {
+app.controller('cameraTabDefaultPageCtrl', function($scope,$cordovaCamera,$firebaseObject,$firebaseArray) {
   var ref = new Firebase("https://project-498244156619960010.firebaseio.com/");
   $scope.firebaseRef = $firebaseArray(ref);
+  var likeStatus = false;
   $scope.takePicture = function() {
        var options = {
            quality : 75,
@@ -23,25 +22,85 @@ angular.module('app.controllers', ["firebase"])
            image = $scope.imgURI;
            alert(image);
        }, function(err) {
-           // An error occured. Show a message to the user
            alert("Error");
        });
    }
    $scope.post = function(){
-     $scope.firebaseRef.$add({
-       name:$scope.clothing_name,
-       price:$scope.price,
-       description:$scope.description,
-       img:$scope.imgURI,
-       likes:0,
-     });
+     if($scope.price > 0 && $scope.clothing_name.length > 0 && $scope.imgURI.length > 0){
+       var theTime = moment().format('MMMM Do YYYY, h:mm a');
+       $scope.firebaseRef.$add({
+         name:$scope.clothing_name,
+         price:$scope.price,
+         description:$scope.description,
+         img:$scope.imgURI,
+         likes:0,
+         time:theTime
+       });
+       swal("Uploaded!", "You've succesfully uploaded your clothing!", "success");
+     }else{
+       swal("Incomplete!", "Take a picture and fill in all the forms.", "error");
+    }
    }
 
-})
+   $scope.payment = function(price,name){
+     swal('Uploaded!', "You've succesfully bought the " + name + " for $" + price, 'success');
+   };
 
-.controller('cartTabDefaultPageCtrl', function($scope) {
-})
+   $scope.like = function(item){
+     if(!likeStatus){
+       console.log(item);
+       var id = item.$id;
+       console.log(id);
+       likes = item.likes + 1;
+       ref.child(id).update({likes:likes});
+       likeStatus = true;
+     }
+     else{
+       console.log(item);
+       var id = item.$id;
+       console.log(id);
+       likes = item.likes - 1;
+       ref.child(id).update({likes:likes});
+       likeStatus = false;
+     }
+   }
 
-.controller('cloudTabDefaultPageCtrl', function($scope) {
+});
 
+app.controller('cartTabDefaultPageCtrl', function($scope) {
+});
+
+app.controller('cloudTabDefaultPageCtrl', function($scope,$ionicPopover) {
+  var ref = new Firebase("https://project-498244156619960010.firebaseio.com/");
+  $scope.firebaseRef = $firebaseArray(ref);
+  $ionicPopover.fromTemplateUrl('templates/popover.html', {
+    scope: $scope,
+  }).then(function(popover) {
+    $scope.popover = popover;
+  });
+
+  $scope.createUser = function(){
+    var email = $scope.email;
+    var password = $scope.password;
+    firebase.auth().createUserWithEmailAndPassword(email,password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      alert(errorCode);
+      var errorMessage = error.message;
+      alert(errorMessage);
+      // ...
+    });
+  }
+  $scope.signIn = function(){
+    var email = $scope.email;
+    var password = $scope.password;
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      var errorCode = error.code;
+      console.log(errorCode);
+      var errorMessage = error.message;
+      console.log(errorMessage);
+      swal("Error", "Invalid Email / Password", "error");
+      // ...
+    });
+  }
 });
--- www/js/routes.js
+++ www/js/routes.js
@@ -7,8 +7,8 @@ angular.module('app.routes', [])
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-    
-  
+
+
 
       .state('tabsController.cameraTabDefaultPage', {
     url: '/page2',
@@ -40,14 +40,15 @@ angular.module('app.routes', [])
     }
   })
 
+
   .state('tabsController', {
     url: '/page1',
     templateUrl: 'templates/tabsController.html',
     abstract:true
   })
 
-$urlRouterProvider.otherwise('/page1/page2')
+$urlRouterProvider.otherwise('/page1/page3')
 
-  
+
 
 });
